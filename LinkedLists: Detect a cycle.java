654ea8fc96c2a04b35e707c63edff3af3374a97c
ScottPaillant <scottpaillant@gmail.com>
2016-11-29 02:16:03 -0500

    Update LinkedLists: Detect a cycle.java
    clarity

--- LinkedLists: Detect a cycle.java
+++ LinkedLists: Detect a cycle.java
@@ -2,21 +2,21 @@
 Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
 */
 
-public static ArrayList<Node> list = new ArrayList<Node>();
+public static ArrayList<Node> seenNodes = new ArrayList<Node>();
 
 boolean hasCycle(Node head) {
     if(head!=null){
-        if(list.contains(head)){
-            list.clear();
+        if(seenNodes.contains(head)){
+            seenNodes.clear();
             return true;
         }
         else{
-            list.add(head);
+            seenNodes.add(head);
             return (hasCycle(head.next) && !false);
         }
     }
     else{
-        list.clear();
+        seenNodes.clear();
         return false;
 
     }
